1d0e6f1d92bfbee026f8954f00062cb23d571c4c
Tom Wendel <tom.wendel@antme.net>
2020-08-05 00:48:57 +0200

    Replaced classic .NET console by a .NET Core console

--- src/OctoConsole/Program.cs
+++ src/OctoConsole/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using OctoPatch;
-using OctoPatch.Exchange;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace OctoConsole
 {
@@ -10,70 +7,14 @@ namespace OctoConsole
     {
         public static void Main(string[] args)
         {
-            var repository = new Repository();
-
-
-            var engine = new Engine(repository);
-
-            var grid = new Grid
-            {
-                Name = "Testgrid",
-                Description = "This is a test grid",
-                NodeInstances = new[]
+            var builder = Microsoft.Extensions.Hosting.Host
+                .CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(b =>
                 {
-                    new NodeInstance
-                    {
-                        Guid = Guid.Parse("{7D01A02E-6ADA-4A60-ACCF-8B74D246EACC}"),
-                        NodeDescription = Guid.Parse("{8AA1AB11-DB28-4098-9999-13A3A47E8A83}"),
-                        Configuration = "{}",
-                    }
-                }.ToList(),
-                WireInstances = new List<WireInstance>()
-            };
+                    b.UseStartup<Startup>();
+                });
 
-            engine.Load(grid);
-
-
-            //var dmxDevice = new DmxDevice();
-            //dmxDevice.Configure(new DmxDeviceConfiguration());
-
-            //var dmxInput = (IObserver<DmxMessage>)dmxDevice.Inputs.First();
-            //dmxInput.OnNext(new DmxMessage { Address = 2, Value = 255 });
-
-            //var midiDevice = new MidiDevice();
-            //midiDevice.Configure(new DeviceConfiguration { DeviceName = "LPD8" });
-
-            //var midiOutput = (IObservable<MidiMessage>)midiDevice.Outputs.First();
-
-            //var midiFilter = new MidiMessageFilter();
-            //var midiFilterInput = (IObserver<MidiMessage>)midiFilter.Inputs.First();
-            //var midiFilterOutput = (IObservable<MidiMessage>)midiFilter.Outputs.First();
-            //midiFilter.Configure(new MidiMessageFilter.Configuration
-            //{
-            //    MessageType = 3,
-            //});
-            //midiOutput.Subscribe(midiFilterInput);
-            //midiFilterOutput.Subscribe(x =>
-            //{
-            //    switch (x.Key)
-            //    {
-            //        case 5:
-            //            dmxInput.OnNext(new DmxMessage { Address = 0, Value = x.Value * 2 });
-            //            break;
-            //        case 6:
-            //            dmxInput.OnNext(new DmxMessage { Address = 1, Value = x.Value * 2 });
-            //            break;
-            //        case 7:
-            //            dmxInput.OnNext(new DmxMessage { Address = 2, Value = x.Value * 2 });
-            //            break;
-            //    }
-
-            //    Console.WriteLine($"Type: {x.MessageType}, Channel: {x.Channel}, Key: {x.Key}, Value: {x.Value}");
-            //});
-
-            Console.ReadLine();
-            //midiDevice.Dispose();
-            //dmxDevice.Dispose();
+            builder.Build().Run();
         }
     }
 }
