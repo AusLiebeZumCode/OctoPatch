aff98da9989a01cd9c29662da4d5255b47894519
Tom Wendel <tom.wendel@antme.net>
2020-08-11 23:03:23 +0200

    Add Debug client

---
+++ src/OctoPatch.DesktopClient/OctoPatchClient.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using OctoPatch.Communication;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OctoPatch.DesktopClient
+{
+    internal sealed class OctoPatchClient : IEngineService
+    {
+        private HubConnection _hubConnection;
+
+        public OctoPatchClient()
+        {
+
+        }
+
+        public async Task Setup(Uri uri, CancellationToken cancellationToken)
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(uri)
+                .Build();
+
+            _hubConnection.Closed += Connection_Closed;
+
+            await _hubConnection.StartAsync(cancellationToken);
+        }
+
+        private Task Connection_Closed(Exception arg)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task<Grid> GetEngineConfiguration(CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync<Grid>(nameof(GetEngineConfiguration), cancellationToken, cancellationToken);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task<IEnumerable<MessageDescription>> GetMessageDescriptions(CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync<IEnumerable<MessageDescription>>(nameof(GetMessageDescriptions), cancellationToken, cancellationToken);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task<string> GetNodeConfiguration(Guid nodeGuid, CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync<string>(nameof(GetNodeConfiguration), nodeGuid, cancellationToken, cancellationToken);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task<IEnumerable<NodeDescription>> GetNodeDescriptions(CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync<IEnumerable<NodeDescription>>(nameof(GetNodeDescriptions), cancellationToken, cancellationToken);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task<string> GetNodeEnvironment(Guid nodeGuid, CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync<string>(nameof(GetNodeEnvironment), nodeGuid, cancellationToken, cancellationToken);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task<IEnumerable<NodeInstance>> GetNodes(CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync<IEnumerable<NodeInstance>>(nameof(GetNodes), cancellationToken, cancellationToken);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task<IEnumerable<WireInstance>> GetWires(CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync<IEnumerable<WireInstance>>(nameof(GetWires), cancellationToken, cancellationToken);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task SetEngineConfiguration(Grid grid, CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync(nameof(SetEngineConfiguration), grid, cancellationToken, cancellationToken);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Task SetNodeConfiguration(Guid nodeGuid, string configuration, CancellationToken cancellationToken)
+        {
+            return _hubConnection.InvokeAsync(nameof(SetNodeConfiguration), nodeGuid, configuration, cancellationToken, cancellationToken);
+        }
+    }
+}
