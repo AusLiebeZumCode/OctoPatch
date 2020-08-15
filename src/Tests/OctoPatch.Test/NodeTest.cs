371c4c91fc44bd718c2da2fdae05b64ccc5ba168
Tom Wendel <tom.wendel@antme.net>
2020-08-16 00:29:42 +0200

    Added test project for OctoPatch lib

---
+++ src/Tests/OctoPatch.Test/NodeTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace OctoPatch.Test
+{
+    public class NodeTest
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
