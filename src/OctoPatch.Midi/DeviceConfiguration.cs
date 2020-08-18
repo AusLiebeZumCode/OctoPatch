0884a9275162c79dc6bbdd5b613c5d04bea38903
Tom Wendel <tom.wendel@antme.net>
2020-08-18 22:02:31 +0200

    Added Midi Plugin to the public project

---
+++ src/OctoPatch.Midi/DeviceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace OctoPatch.Midi
+{
+    /// <summary>
+    /// Configuration set for the MIDI Devices
+    /// </summary>
+    public sealed class DeviceConfiguration : INodeConfiguration
+    {
+        /// <summary>
+        /// Gets or sets the name of the device that's represented
+        /// </summary>
+        public string DeviceName { get; set; }
+    }
+}
