cfa15649285a502f5c2037a08854d109afddbf9e
Tom Wendel <tom.wendel@antme.net>
2020-08-27 02:38:21 +0200

    Rebrand core project as the basic plugin for system types

---
+++ src/Core/OctoPatch.Core/CorePlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using OctoPatch.Server;
+
+namespace OctoPatch.Core
+{
+    /// <summary>
+    /// Plugin for all basic types delivered with octo patch
+    /// </summary>
+    public sealed class CorePlugin : IPlugin
+    {
+        public string Name => "Core components";
+        public string Description => "Plugin contains all basic types required for octo patch";
+        public Version Version => new Version(1, 0, 0);
+        public IEnumerable<NodeDescription> GetNodeDescriptions()
+        {
+            return Enumerable.Empty<NodeDescription>();
+        }
+
+        public IEnumerable<ComplexTypeDescription> GetTypeDescriptions()
+        {
+            return Enumerable.Empty<ComplexTypeDescription>();
+        }
+
+        public Task<INode> CreateNode(Guid nodeDescriptionGuid, Guid nodeId, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
