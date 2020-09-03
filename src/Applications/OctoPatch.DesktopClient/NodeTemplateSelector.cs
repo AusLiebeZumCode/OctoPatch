6e60db712f773da813484ad28e7df6e739fdef9d
Tom Wendel <tom.wendel@antme.net>
2020-09-03 17:20:16 +0200

    Created a template selector to select custom property boxes for specific nodes
    still hardcoded for midi plugin to see how this works

---
+++ src/Applications/OctoPatch.DesktopClient/NodeTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using OctoPatch.DesktopClient.Models;
+
+namespace OctoPatch.DesktopClient
+{
+    public sealed class NodeTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate MidiDeviceTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is MidiDeviceModel)
+            {
+                return MidiDeviceTemplate;
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
