6632a977c8a9c481793a01a179d886306372ebbf
Tom Wendel <tom.wendel@antme.net>
2020-09-27 01:07:15 +0200

    Fixed visibility of property boxes with converters

---
+++ src/Applications/OctoPatch.DesktopClient/Converters/InRangeToVisibilityConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OctoPatch.DesktopClient.Converters
+{
+    /// <summary>
+    /// Checks if the given value is within the range
+    /// </summary>
+    [ValueConversion(typeof(int), typeof(Visibility))]
+    public sealed class InRangeToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Minimum value
+        /// </summary>
+        public int MinValue { get; set; }
+
+        /// <summary>
+        /// Maximum value
+        /// </summary>
+        public int MaxValue { get; set; }
+
+        public InRangeToVisibilityConverter()
+        {
+            MinValue = int.MinValue;
+            MaxValue = int.MaxValue;
+        }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is int input))
+            {
+                return Visibility.Collapsed;
+            }
+
+            return input >= MinValue && input <= MaxValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Applications/OctoPatch.DesktopClient/Converters/NodeToVisibilityConverter.cs
+++ src/Applications/OctoPatch.DesktopClient/Converters/NodeToVisibilityConverter.cs
@@ -6,12 +6,87 @@ using OctoPatch.DesktopClient.Models;
 
 namespace OctoPatch.DesktopClient.Converters
 {
+    /// <summary>
+    /// Converts selected tree node type into visibility
+    /// </summary>
     [ValueConversion(typeof(NodeModel), typeof(Visibility))]
     class NodeToVisibilityConverter : IValueConverter
     {
+        /// <summary>
+        /// show up when common node is selected
+        /// </summary>
+        public bool CommonNode { get; set; }
+
+        /// <summary>
+        /// show up when attached node is selected
+        /// </summary>
+        public bool AttachedNode { get; set; }
+
+        /// <summary>
+        /// show up when splitter node is selected
+        /// </summary>
+        public bool SplitterNode { get; set; }
+
+        /// <summary>
+        /// show up when collector node is selected
+        /// </summary>
+        public bool CollectorNode { get; set; }
+
+        /// <summary>
+        /// show up when input connector is selected
+        /// </summary>
+        public bool InputConnector { get; set; }
+
+        /// <summary>
+        /// show up when output connector is selected
+        /// </summary>
+        public bool OutputConnector { get; set; }
+
+        /// <summary>
+        /// show up when wire is selected
+        /// </summary>
+        public bool Wire { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value is CommonNodeModel || value is AttachedNodeModel || value is SplitterNodeModel || value is CollectorNodeModel ? Visibility.Visible : Visibility.Collapsed;
+            var visible = false;
+
+            if (CommonNode && value is CommonNodeModel)
+            {
+                visible = true;
+            }
+
+            if (AttachedNode && value is AttachedNodeModel)
+            {
+                visible = true;
+            }
+
+            if (SplitterNode && value is SplitterNodeModel)
+            {
+                visible = true;
+            }
+
+            if (CollectorNode && value is CollectorNodeModel)
+            {
+                visible = true;
+            }
+
+            if (InputConnector && value is InputNodeModel)
+            {
+                visible = true;
+            }
+
+            if (OutputConnector && value is OutputNodeModel)
+            {
+                visible = true;
+            }
+
+            if (Wire && value is WireNodeModel)
+            {
+                visible = true;
+            }
+
+            return visible ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
