444f56c29ae9f1410c5956e86ee02b8196383805
Tom Wendel <tom.wendel@antme.net>
2020-10-06 23:27:49 +0200

    Added Config view and model for attached midi nodes

---
+++ src/Applications/OctoPatch.DesktopClient/Models/MidiAttachedNodeModel.cs
@@ -0,0 +1,59 @@
+﻿using OctoPatch.Plugin.Midi;
+
+namespace OctoPatch.DesktopClient.Models
+{
+    /// <summary>
+    /// Configuration model for all kind of attached nodes of the MIDI Device
+    /// </summary>
+    public sealed class MidiAttachedNodeModel : NodeConfigurationModel<AttachedNodeConfiguration, EmptyEnvironment>
+    {
+        private int _channel;
+
+        /// <summary>
+        /// Gets or sets the channel of the message filter
+        /// </summary>
+        public int Channel
+        {
+            get => _channel;
+            set
+            {
+                _channel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _key;
+
+        /// <summary>
+        /// Gets or sets the key of the message filter
+        /// </summary>
+        public int Key
+        {
+            get => _key;
+            set
+            {
+                _key = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected override void OnSetup(EmptyEnvironment environment)
+        {
+        }
+
+        protected override void OnSetConfiguration(AttachedNodeConfiguration configuration)
+        {
+            Channel = configuration.Channel;
+            Key = configuration.Key;
+        }
+
+        protected override AttachedNodeConfiguration OnGetConfiguration()
+        {
+            return new AttachedNodeConfiguration
+            {
+                Channel = Channel,
+                Key = Key
+            };
+        }
+    }
+}
