0c5a15fc32b6abd3561560bf246654da63eef89e
Tom Wendel <tom.wendel@antme.net>
2020-10-12 16:21:07 +0200

    Start declaration of a proper adapter

--- src/Core/OctoPatch/Adapter.cs
+++ src/Core/OctoPatch/Adapter.cs
@@ -1,4 +1,9 @@
-﻿namespace OctoPatch
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OctoPatch
 {
     /// <summary>
     /// Base class for all kind of adapters
@@ -9,6 +14,36 @@
         where TConfiguration : IConfiguration
         where TEnvironment : IEnvironment
     {
+        private TConfiguration _configuration;
+
+        /// <summary>
+        /// Internal reference to the current configuration
+        /// </summary>
+        protected TConfiguration Configuration
+        {
+            get => _configuration;
+            private set
+            {
+                _configuration = value;
+                ConfigurationChanged?.Invoke(this, GetEnvironment());
+            }
+        }
+
+        private TEnvironment _environment;
+
+        /// <summary>
+        /// Internal reference to the current environment
+        /// </summary>
+        protected TEnvironment Environment
+        {
+            get => _environment;
+            private set
+            {
+                _environment = value;
+                EnvironmentChanged?.Invoke(this, GetEnvironment());
+            }
+        }
+
         protected Adapter(IInputConnector input, IOutputConnector output)
         {
         }
@@ -17,5 +52,66 @@
         {
 
         }
+
+        /// <summary>
+        /// Sets a new configuration to the adapter
+        /// </summary>
+        /// <param name="configuration">serialized configuration</param>
+        /// <param name="cancellationToken">cancellation token</param>
+        public async Task SetConfiguration(string configuration, CancellationToken cancellationToken)
+        {
+            // Deserialization of configuration
+            TConfiguration config;
+            try
+            {
+                config = JsonConvert.DeserializeObject<TConfiguration>(configuration);
+            }
+            catch (JsonSerializationException ex)
+            {
+                throw new ArgumentException("could not deserialize configuration", ex);
+            }
+            catch (JsonReaderException ex)
+            {
+                throw new ArgumentException("could not read configuration", ex);
+            }
+
+            // Initialize
+            await OnInitialize(config, cancellationToken);
+
+            // Write back values
+            Configuration = config;
+        }
+
+        /// <summary>
+        /// Gets a call when node is in setup
+        /// </summary>
+        /// <param name="configuration">configuration</param>
+        /// <param name="cancellationToken">cancellation token</param>
+        protected virtual Task OnInitialize(TConfiguration configuration, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public string GetEnvironment()
+        {
+            return JsonConvert.SerializeObject(Environment);
+        }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public string GetConfiguration()
+        {
+            return JsonConvert.SerializeObject(Configuration);
+        }
+
+        /// <inheritdoc />
+        public event Action<IAdapter, string> ConfigurationChanged;
+        
+        /// <inheritdoc />
+        public event Action<IAdapter, string> EnvironmentChanged;
     }
 }
--- src/Core/OctoPatch/IAdapter.cs
+++ src/Core/OctoPatch/IAdapter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace OctoPatch
 {
@@ -7,5 +9,33 @@ namespace OctoPatch
     /// </summary>
     public interface IAdapter : IDisposable
     {
+        /// <summary>
+        /// Returns the current environment serialized as string
+        /// </summary>
+        /// <returns>serialized environment</returns>
+        string GetEnvironment();
+
+        /// <summary>
+        /// Returns the current configuration serialized as string
+        /// </summary>
+        /// <returns>serialized configuration</returns>
+        string GetConfiguration();
+
+        /// <summary>
+        /// Sets a new configuration to the adapter
+        /// </summary>
+        /// <param name="configuration">serialized configuration</param>
+        /// <param name="cancellationToken">cancellation token</param>
+        Task SetConfiguration(string configuration, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Gets a call when the current configuration changes.
+        /// </summary>
+        event Action<IAdapter, string> ConfigurationChanged;
+
+        /// <summary>
+        /// Gets a call when the current environment changes.
+        /// </summary>
+        event Action<IAdapter, string> EnvironmentChanged;
     }
 }
