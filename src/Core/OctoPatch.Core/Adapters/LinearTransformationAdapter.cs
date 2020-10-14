626b8a9f9f30600fce1810dfafef553f67fecfb7
Tom Wendel <tom.wendel@antme.net>
2020-10-14 02:03:37 +0200

    Apply adapter changes and finish linear transformation adapter

--- src/Core/OctoPatch.Core/Adapters/LinearTransformationAdapter.cs
+++ src/Core/OctoPatch.Core/Adapters/LinearTransformationAdapter.cs
@@ -4,7 +4,7 @@ using OctoPatch.ContentTypes;
 
 namespace OctoPatch.Core.Adapters
 {
-    public sealed class LinearTransformationAdapter : Adapter<LinearTransformationAdapter.Config, EmptyEnvironment>
+    public sealed class LinearTransformationAdapter : Adapter<double, LinearTransformationAdapter.Config, EmptyEnvironment>
     {
         #region definitions
 
@@ -12,19 +12,50 @@ namespace OctoPatch.Core.Adapters
             AdapterDescription.Create<LinearTransformationAdapter>(
                 Guid.Parse(CorePlugin.PluginId), "Linear Adapter", "Linear Adapter")
                 .AddSupportedTypeCombination<IntegerContentType, IntegerContentType>()
-                .AddSupportedTypeCombination<FloatContentType, IntegerContentType>()
                 .AddSupportedTypeCombination<IntegerContentType, FloatContentType>()
+                .AddSupportedTypeCombination<FloatContentType, IntegerContentType>()
                 .AddSupportedTypeCombination<FloatContentType, FloatContentType>();
 
         #endregion
 
         public LinearTransformationAdapter(IWire wire) : base(wire)
         {
+            RegisterImporter<int>(ImportInteger);
+            RegisterImporter<float>(ImportFloat);
+            RegisterExporter<int>(ExportInteger);
+            RegisterExporter<float>(ExportFloat);
         }
 
-        protected override Message Handle(Message message)
+        private double ImportInteger(int input)
         {
-            return message;
+            var integerInput = (IntegerContentType)Input.ContentType;
+            var integerMin = integerInput.MinimumValue ?? int.MinValue;
+            var integerMax = integerInput.MaximumValue ?? int.MaxValue;
+            return (1.0 / (integerMax - integerMin)) * (input - integerMin);
+        }
+
+        private double ImportFloat(float input)
+        {
+            var floatInput = (FloatContentType)Input.ContentType;
+            var floatMin = floatInput.MinimumValue ?? float.MinValue;
+            var floatMax = floatInput.MaximumValue ?? float.MaxValue;
+            return (1.0 / (floatMax - floatMin)) * (input - floatMin);
+        }
+
+        private int ExportInteger(double input)
+        {
+            var integerOutput = (IntegerContentType)Output.ContentType;
+            var integerMin = integerOutput.MinimumValue ?? int.MinValue;
+            var integerMax = integerOutput.MaximumValue ?? int.MaxValue;
+            return (int)((1.0 / (integerMax - integerMin)) * input) + integerMin;
+        }
+
+        private float ExportFloat(double input)
+        {
+            var floatInput = (FloatContentType)Output.ContentType;
+            var floatMin = floatInput.MinimumValue ?? float.MinValue;
+            var floatMax = floatInput.MaximumValue ?? float.MaxValue;
+            return (float)((1.0 / (floatMax - floatMin)) * input) + floatMin;
         }
 
         public sealed class Config : IConfiguration
