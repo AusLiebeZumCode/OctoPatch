8e122750e266f40bebb8e5a4f1222e03dbe67d5b
Tom Wendel <tom.wendel@antme.net>
2020-10-27 23:33:53 +0100

    Include a simple control for connectors

---
+++ src/Applications/OctoPatch.DesktopClient/Views/ConnectorView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OctoPatch.ContentTypes;
+
+namespace OctoPatch.DesktopClient.Views
+{
+    /// <summary>
+    /// Interaction logic for ConnectorView.xaml
+    /// </summary>
+    public partial class ConnectorView : UserControl
+    {
+        // Using a DependencyProperty as the backing store for ContentType.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContentTypeProperty =
+            DependencyProperty.Register("ContentType", typeof(ContentType), typeof(ConnectorView));
+
+        [Bindable(true)]
+        public ContentType ContentType
+        {
+            get => (ContentType)GetValue(ContentTypeProperty);
+            set => SetValue(ContentTypeProperty, value);
+        }
+
+        public ConnectorView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            if (ContentType is ComplexContentType complexContentType)
+            {
+                var x = complexContentType.Type;
+            }
+
+            base.OnMouseEnter(e);
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+        }
+    }
+}
